5f6c97fa2710337f4a38a1cd14e400b465425082
harshaldesh <114969963+harshaldesh@users.noreply.github.com>
2025-03-01 20:17:54 +0530

    Update Python1.py

--- Python1.py
+++ Python1.py
@@ -1,7 +1,8 @@
 print("Hello World")
-def factorail(n):
-  if n==0 or n==1:
-    return 1
-  return m*factorial(n-1)
-  num=int(input("Enter the Number:"))
-  print("Factorial of {num} is {factorial(num)}")
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is {factorial(num)}")
